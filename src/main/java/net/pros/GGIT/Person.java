5e0f8e94e0d8eb9f19f6baf4eabb460c1c0a25b7
roma <kvarchiyarom@gmail.com>
2020-12-25 23:57:06 +0300

    add new class person

---
+++ src/main/java/net/pros/GGIT/Person.java
@@ -0,0 +1,33 @@
+package net.pros.GGIT;
+
+public class Person extends BaseEntity{
+
+    private String firstName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + super.getId() + '\'' +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
